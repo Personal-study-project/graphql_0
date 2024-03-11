791eb05dfffd91dc0224bf1629fda08032941b9e
jae-hwan-kim <woghks7859@gmail.com>
2024-03-11 14:25:56 +0900

    feat: enums, list, 스칼라, 객체 타입 학습 및 적용

--- 3-2-types/index.js
+++ 3-2-types/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('apollo-server')
 
+const enums = require('./typedefs-resolvers/_enums')
 const queries = require('./typedefs-resolvers/_queries')
 const mutations = require('./typedefs-resolvers/_mutations')
 const equipments = require('./typedefs-resolvers/equipments')
@@ -8,6 +9,7 @@ const supplies = require('./typedefs-resolvers/supplies')
 const typeDefs = [
     queries,
     mutations,
+    enums,
     equipments.typeDefs,
     supplies.typeDefs
 ]
